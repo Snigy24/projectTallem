d69b197fb69511154d9e5151834f295a24d6e753
Tiago Alves <tfaon@sapo.pt>
2018-04-14 16:11:56 +0100

    Add first build and unity files

---
+++ ProjectTellem_UnityFiles/14-04-2018_15h-20m/TestePlatformer2D/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    private GameObject cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update () {
+            transform.position = new Vector2(cam.transform.position.x + 6, cam.transform.position.y + 4);
+    }
+}
